3ec1192b346e58959df7b4c90ea8ece1ab9a59c2
Fredrik Lugg <31255680+fredriklugg@users.noreply.github.com>
2020-03-23 11:56:27 +0100

    Added more markers and update function

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import ExpoTHREE, { THREE } from 'expo-three';
 import React from 'react';
 import {Platform} from "react-native";
 
+let cordsArray = [{lat: 59.9139, lon: 10.7522}, {lat:51.508476 ,lon: -0.103321}, {lat:48.5124 ,lon: 2.103321}, {lat:69.5836 ,lon: 23.1745}];
+
+
 export default class App extends React.Component {
     render() {
         // Create an `ExpoGraphics.View` covering the whole screen, tell it to call our
@@ -30,7 +33,12 @@ export default class App extends React.Component {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
         this.camera.position.z = 5;
-        const geometry = new THREE.SphereGeometry(1.5, 42, 42);
+
+        //this.controls = new THREE.OrbitControls(this.camera);
+
+        const earthGeometry = new THREE.SphereGeometry(2, 42, 42);
+        const markerGeometry = new THREE.SphereGeometry(0.02,50,50);
+
 
 
         const texture = require('./assets/earth2.png');
@@ -46,22 +54,30 @@ export default class App extends React.Component {
             bumpMap = await ExpoTHREE.loadTextureAsync({asset: bump});
         }
 
-        const material = new THREE.MeshPhongMaterial({
+        const earthMaterial = new THREE.MeshPhongMaterial({
             map,
             bumpMap,
             bumpScale
         });
+        const markerMaterial = new THREE.MeshPhongMaterial({
+            color: 0xff0000
+        });
 
-        this.sphere = new THREE.Mesh(geometry, material);
-        this.scene.add(this.sphere);
+        this.sphere = new THREE.Mesh(earthGeometry, earthMaterial);
 
-        // set earth start position
-        this.sphere.rotation.x = 0.3;
-        this.sphere.rotation.y = -2;
+        //placeObjectOnPlanet(this.marker, 59.9139, 10.7522, 2);
+
+        this.group = new THREE.Object3D();
+        this.group.add(this.sphere);
+        createMarkes(this.group, markerGeometry, markerMaterial);
+
+        this.scene.add(this.group);
+
+        // set earth start rotation
+        this.group.rotation.x = 0.3;
+        this.group.rotation.y = -2;
         //this.sphere.position.x = 1.5;
         //this.sphere.position.z = 2;
-
-
 
 
         this.scene.add(new THREE.AmbientLight(0x404040));
@@ -72,21 +88,28 @@ export default class App extends React.Component {
     };
 
     onRender = delta => {
-        this.sphere.rotation.y += 0.2 * delta;
+        this.group.rotation.y += 0.2 * delta;
+        //this.controls.update();
         this.renderer.render(this.scene, this.camera);
     };
+
+}
+function createMarkes(group, geo, mat){
+    for (let i = 0; i < cordsArray.length; i++) {
+        let marker = new THREE.Mesh(geo, mat);
+
+        placeObjectOnPlanet(marker, cordsArray[i].lat, cordsArray[i].lon);
+        group.add(marker)
+    }
 }
 
-function calcPosFromLatLonRad(lat,lon,radius){
-
-    var phi   = (90-lat)*(Math.PI/180);
-    var theta = (lon+180)*(Math.PI/180);
-
-    x = -((radius) * Math.sin(phi)*Math.cos(theta));
-    z = ((radius) * Math.sin(phi)*Math.sin(theta));
-    y = ((radius) * Math.cos(phi));
-
-
-    console.log([x,y,z]);
-    return [x,y,z];
+function placeObjectOnPlanet(object, lat, lon) {
+    var latRad = lat * (Math.PI / 180);
+    var lonRad = -lon * (Math.PI / 180);
+    object.position.set(
+        (Math.cos(latRad) * Math.cos(lonRad) * 2)+0.03,
+        (Math.sin(latRad) * 2),
+        (Math.cos(latRad) * Math.sin(lonRad) * 2)
+    );
+    object.rotation.set(0.0, -lonRad, latRad - Math.PI * 0.5);
 }
